ced2799434ceade6b280c5886086d7edf33c9cc9
NewDeveloper888 <anamahmoudyass006@gmail.com>
2026-08-16 06:09:55 +0300

    Stage 3: create with validation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, status
-
+from pydantic import BaseModel
 
 # create app instance
 app = FastAPI(
@@ -13,6 +13,8 @@ tasks = [
     {"id": 3, "title": "Complete assignment", "done": False},
 ]
 
+class TaskCreate(BaseModel):
+    title: str
 
 #in stage 1 and 0:
 @app.get('/', summary="root endpoint")
@@ -47,6 +49,28 @@ def get_task_by_id(id: int):
         detail=f"Task {id} not found"
     )
 
+# --- Stage 3: Create Endpoint ---
+@app.post("/tasks", status_code=status.HTTP_201_CREATED, summary="Create a new task")
+def create_task(payload: TaskCreate):
+    # validate from spaces on the title 
+    cleaned_title = payload.title.strip()
+    # if the title is empty 
+    if not cleaned_title:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Title cannot be empty"
+        )
+    
+    # calculate the new id : 
+    new_id = max([t["id"] for t in tasks], default=0) + 1
+    # create the new task :
+    new_task = {
+        "id": new_id,
+        "title": cleaned_title,
+        "done": False
+    }
+    tasks.append(new_task)
+    return new_task
 
 
     
